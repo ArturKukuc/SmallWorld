b3c481611be4b7d275fd3ede1a2eec2dded5e8fd
Artur Kukuć <akukuc8@gmail.com>
2020-12-07 22:55:22 +0100

    Race class created - fields and constructors

---
+++ src/Race.java
@@ -0,0 +1,30 @@
+public abstract class Race {
+
+    private int startingUnits;
+    private boolean isExtinct;
+
+    public Race(int startingUnits, boolean isExtinct){
+        this.startingUnits = startingUnits;
+        this.isExtinct = isExtinct;
+    }
+
+    public Race(String name, int startingUnits){
+        this(startingUnits, false);
+    }
+
+    public int getStartingUnits() {
+        return startingUnits;
+    }
+
+    public boolean isExtinct() {
+        return isExtinct;
+    }
+
+    public void extinctRace() {
+        isExtinct = true;
+    }
+
+    public void showRaceStats(){
+        System.out.println("Race: ");
+    }
+}
